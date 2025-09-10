c1d38383df218dde46796a959d1ff90e7f38b5df
avatlyapp-prog <avatly.app@gmail.com>
2025-09-10 22:52:12 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,24 @@
-const CACHE_NAME = 'avatly-cache-v1';
-const ASSETS = [
+const CACHE_NAME='avatly-v01';
+const ASSETS=[
   './',
   './index.html',
   './styles.css',
   './app.js',
   './manifest.webmanifest',
-  './icon-192.png',
-  './icon-512.png'
+  './assets/icon-192.png',
+  './assets/icon-512.png'
 ];
 
-// Install service worker
-self.addEventListener('install', e => {
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate',e=>{
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(ASSETS);
-    })
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))
   );
 });
 
-// Activate service worker
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
-    )
-  );
-});
-
-// Fetch assets
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
