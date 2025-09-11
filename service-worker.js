686c9b6b951162ccbf31d3b2d45d35d3db51923a
avatlyapp-prog <avatly.app@gmail.com>
2025-09-11 13:57:28 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,28 @@
-// Avatly PWA cache – bump la versione ad ogni release
-const CACHE_NAME='avatly-v05';
-const ASSETS=[
+// Avatly SW v0.4
+const CACHE_NAME = 'avatly-v04';
+const ASSETS = [
   './',
-  './index.html',
-  './styles.css',
-  './app.js',
+  './index.html?v=4',
+  './styles.css?v=4',
+  './app.js?v=4',
   './manifest.webmanifest',
   './icon-192.png',
   './icon-512.png'
 ];
 
-self.addEventListener('install',e=>{
+self.addEventListener('install', e=>{
   e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
 });
-
-self.addEventListener('activate',e=>{
-  e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))
-  );
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(
+    keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))
+  )));
+  self.clients.claim();
 });
-
-self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+self.addEventListener('fetch', e=>{
+  const url = new URL(e.request.url);
+  if(url.origin===location.origin){
+    e.respondWith(caches.match(e.request).then(res=>res||fetch(e.request)));
+  }
 });
